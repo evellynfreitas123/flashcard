d90df8f380a376970ae8138771c839268736a24c
evellynfreitas123 <168579519+evellynfreitas123@users.noreply.github.com>
2024-11-12 14:16:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'HTML é uma linguagem de marcação de hipertexto utilizada na criação de documentos e páginas da web'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Inglaterra?',
+    'A capital da França é Londres'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'CSS é a sigla para o termo em inglês Cascading Style Sheets que, traduzido para o português, significa Folha de Estilo em Cascatas.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'Amor em ingles é Love'
+)
